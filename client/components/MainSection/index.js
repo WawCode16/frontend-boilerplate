afcbbb66c07ddcaec064b5b78c5282f4bd59474c
Islam <islomjon-yunusov@mail.ru>
2016-10-22 18:45:40 +0200

    map component

--- client/components/MainSection/index.js
+++ client/components/MainSection/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import HeaderLogo from '../HeaderLogo/index'
+import MapElement from '../MapElement/index'
 import AddressInput from '../AddressInput/index'
 import style from './style.scss'
 
@@ -10,12 +11,24 @@ class MainSection extends Component {
     this.state = {}
   }
 
+    renderMaps() {
+        return (
+            <MapElement
+                onMapLoad={_.noop}
+                onMapClick={_.noop}
+                markers={[]}
+                onMarkerRightClick={_.noop}
+            />
+        )
+    }
+
 
   render() {
     const {actions} = this.props
     return (
       <div className="main-section">
         <HeaderLogo />
+        {this.renderMaps()}
       </div>
     )
   }
