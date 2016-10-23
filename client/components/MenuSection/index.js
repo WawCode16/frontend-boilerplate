46b5a69e6415675c15071df1ea3d0db54d2aff6c
Islam <islomjon-yunusov@mail.ru>
2016-10-23 16:22:45 +0200

    Design

--- client/components/MenuSection/index.js
+++ client/components/MenuSection/index.js
@@ -14,6 +14,18 @@ class MenuSection extends Component {
     return (
       <div className="menu-section">
         <AddressInput onSearch={::this.search}/>
+        <div id ="header-top">
+            <nav id="home-nav" className="nav-wide" role="navigation">
+                <ul>
+                    <li className="home"><a href="#">HOME</a></li>
+                    <li className="about rightflow active"><a href="#">RAPORT</a></li>
+                    <li className="haunting"><a href="#">EDUKACJA!</a></li>
+                    <li className="party"><a href="#">KULINARIA</a></li>
+                    <li className="contact"><a href="#">TERENY ZIELONE</a></li>
+                    <li className="contact"><a href="#">TRANSPORT</a></li>
+                </ul>
+            </nav>
+        </div>
       </div>
     )
   }
